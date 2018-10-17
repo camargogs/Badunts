26b60818d242b04d31de7a04514511a5b2f05710
Camargo <gomes.camargo22@gmail.com>
2018-10-17 16:37:50 -0300

    merged the sprint 0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
+// import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 import { LoadingComponent } from './loading/loading.component';
 import { MenuComponent } from './menu/menu.component';
@@ -11,7 +11,6 @@ import { routing } from './app.routing';
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent,
     LoginComponent,
     LoadingComponent,
     MenuComponent
