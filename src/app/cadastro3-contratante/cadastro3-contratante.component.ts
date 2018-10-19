e166ad16b61442db4497718ab314774fe4d61950
Adonai Diógenes França Silva <adonaifranca@gmail.com>
2018-10-18 23:10:53 -0300

    Ajuste do botão da primeira tela de cadastro que estava desalinhado. E envio de arquivos que não tinham sido enviados

---
+++ src/app/cadastro3-contratante/cadastro3-contratante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro3-contratante',
+  templateUrl: './cadastro3-contratante.component.html',
+  styleUrls: ['../cadastro/cadastro.component.css']
+})
+export class Cadastro3ContratanteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/cadastro3-musico/cadastro3-musico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro3-musico',
+  templateUrl: './cadastro3-musico.component.html',
+  styleUrls: ['../cadastro/cadastro.component.css']
+})
+export class Cadastro3MusicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
