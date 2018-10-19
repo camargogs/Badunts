488cb915689b62a07720105030bb54cb9a8a8c03
Adonai Diógenes França Silva <adonaifranca@gmail.com>
2018-10-19 04:25:58 -0300

    Inicio da primeira tela de perfil do musico. Componente de navegador tipo Tab.

---
+++ src/app/perfil-musico/perfil-musico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-musico',
+  templateUrl: './perfil-musico.component.html',
+  styleUrls: ['./perfil-musico.component.css']
+})
+export class PerfilMusicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
