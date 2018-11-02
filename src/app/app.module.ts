714016b483f0512511056889147c39ca18228fc8
Adonai Diógenes França Silva <adonaifranca@gmail.com>
2018-11-02 00:28:21 -0200

    Telas do perfil do musico (SEM MODO DE EDIÇÃO) -tela de agenda está vazia-

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-// import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 import { LoadingComponent } from './loading/loading.component';
 import { MenuComponent } from './menu/menu.component';
 import { routing } from './app.routing';
 import { PerfilMusicoComponent } from './perfil-musico/perfil-musico.component';
+import { PerfilMusicoShowsComponent } from './perfil-musico-shows/perfil-musico-shows.component';
+import { PerfilMusicoAgendaComponent } from './perfil-musico-agenda/perfil-musico-agenda.component';
+import { PerfilMusicoAvaliacoesComponent } from './perfil-musico-avaliacoes/perfil-musico-avaliacoes.component';
 
 
 @NgModule({
@@ -17,6 +19,9 @@ import { PerfilMusicoComponent } from './perfil-musico/perfil-musico.component';
     LoadingComponent,
     MenuComponent,
     PerfilMusicoComponent,
+    PerfilMusicoShowsComponent,
+    PerfilMusicoAgendaComponent,
+    PerfilMusicoAvaliacoesComponent,
    
   ],
   imports: [
