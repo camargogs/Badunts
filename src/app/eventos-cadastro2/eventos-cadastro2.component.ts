bc738638d728b88522358b10c10585293762d951
Adonai Diógenes França Silva <adonaifranca@gmail.com>
2018-11-11 21:58:21 -0200

    segunda tela de cadastro de evento impletmentada. troca de algumas imagens pelo svg (não foi feito em todas as imagens ainda)

--- src/app/eventos-cadastro2/eventos-cadastro2.component.ts
+++ src/app/eventos-cadastro2/eventos-cadastro2.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos-cadastro2',
   templateUrl: './eventos-cadastro2.component.html',
-  styleUrls: ['./eventos-cadastro2.component.css']
+  styleUrls: ['../eventos/eventos.component.css']
 })
 export class EventosCadastro2Component implements OnInit {
 
