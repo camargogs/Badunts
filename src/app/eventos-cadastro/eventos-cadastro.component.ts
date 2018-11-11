8f9438e88012eb77d5b20e4ee3a38ba5d575411d
Adonai Diógenes França Silva <adonaifranca@gmail.com>
2018-11-11 18:35:15 -0200

    primeira tela de cadastro do evento

--- src/app/eventos-cadastro/eventos-cadastro.component.ts
+++ src/app/eventos-cadastro/eventos-cadastro.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos-cadastro',
   templateUrl: './eventos-cadastro.component.html',
-  styleUrls: ['./eventos-cadastro.component.css']
+  styleUrls: ['../eventos/eventos.component.css']
 })
 export class EventosCadastroComponent implements OnInit {
 
