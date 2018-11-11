5daa1834d6b5fac30355db5406b565618218f4ff
Alquimara <alquimara.a.braz@gmail.com>
2018-11-11 15:15:55 -0200

    perfil contratante

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,20 @@
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 import { Routes, RouterModule } from "@angular/router";
+import { PerfilMusicoComponent } from './perfil-musico/perfil-musico.component';
+import { PerfilMusicoShowsComponent } from './perfil-musico-shows/perfil-musico-shows.component';
+import { PerfilMusicoAgendaComponent } from './perfil-musico-agenda/perfil-musico-agenda.component';
+import { PerfilMusicoAvaliacoesComponent } from './perfil-musico-avaliacoes/perfil-musico-avaliacoes.component';
+
 
 const APP_ROUTES: Routes = [
 {path:"", redirectTo:"login", pathMatch:"full"},
 {path:"login", component:LoginComponent},
-{path:"menu", component:MenuComponent}
+{path:"menu", component:MenuComponent},
+{path:"perfil",component:PerfilMusicoComponent},
+{path:"shows", component:PerfilMusicoShowsComponent},
+{path:"agenda", component:PerfilMusicoAgendaComponent},
+{path:"avaliacoes", component:PerfilMusicoAvaliacoesComponent}
 ]
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
--- src/app/perfil-musico/perfil-musico.component.ts
+++ src/app/perfil-musico/perfil-musico.component.ts
@@ -6,10 +6,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-musico.component.css']
 })
 export class PerfilMusicoComponent implements OnInit {
+  tagvideo:boolean;
+  tagaudio:boolean;
+  tagtexto:boolean;
+  tagfoto:boolean;
+  tagtodo:boolean;
+  audio:string;
+  video:string;
+  foto:string;
+  texto:string;
+  todo:string;
+
 
   constructor() { }
 
   ngOnInit() {
   }
+  alterarTagVideo(){
+    if((this.tagvideo=true)&& (this.video = 'block')){
+      this.tagaudio=false;
+      this.tagfoto=false;
+      this.tagtexto = false;
+      this.tagtodo = false;
+      this.foto = 'none';
+      this.texto = 'none';
+      this.audio = 'none';
+      this.todo = 'none';
+     
+
+    }
+    
+    
+  }
+  alterarTagAudio(){
+    if((this.tagaudio=true)&&(this.audio = 'block')){
+      this.tagvideo=false;
+      this.tagfoto=false;
+      this.tagtexto = false;
+      this.tagtodo = false;
+      this.foto = 'none';
+      this.texto = 'none';
+      this.video = 'none';
+      this.todo = 'none';
+
+    }
+    
+  
+  }
+  alterarTagFoto(){
+    if((this.tagfoto=true)&&(this.foto = 'block')){
+      this.tagaudio=false;
+      this.tagvideo=false;
+      this.tagtexto = false;
+      this.tagtodo = false;
+      this.audio = 'none';
+      this.texto = 'none';
+      this.video = 'none';
+      this.todo = 'none';
+    }
+    
+   
+  }
+  alterarTagTexto(){
+    if((this.tagtexto =true)&&(this.texto = 'block')){
+      this.tagaudio=false;
+      this.tagfoto=false;
+      this.tagvideo = false;
+      this.tagtodo = false;
+      this.audio = 'none';
+      this.foto = 'none';
+      this.video = 'none';
+      this.todo = 'none';
+
+    }
+  }
+    alterarTagTodo(){
+      if((this.tagtodo=true)&&(this.todo = 'block')){
+        this.tagaudio=false;
+        this.tagfoto=false;
+        this.tagvideo = false;
+        this.tagtexto = false;
+        this.audio = 'none';
+        this.foto = 'none';
+        this.video = 'none';
+        this.texto = 'none';
+  
+      }
+  
+}
 
 }
