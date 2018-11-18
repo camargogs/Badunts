ade4358373434b81fe7303acff27ab939528579a
Camargo <gomes.camargo22@gmail.com>
2018-11-18 14:30:20 -0300

    adding alterations

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,11 +5,22 @@ import { PerfilMusicoComponent } from './perfil-musico/perfil-musico.component';
 import { PerfilMusicoShowsComponent } from './perfil-musico-shows/perfil-musico-shows.component';
 import { PerfilMusicoAgendaComponent } from './perfil-musico-agenda/perfil-musico-agenda.component';
 import { PerfilMusicoAvaliacoesComponent } from './perfil-musico-avaliacoes/perfil-musico-avaliacoes.component';
-
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { Cadastro3ContratanteComponent } from './cadastro3-contratante/cadastro3-contratante.component';
+import { Cadastro2Component } from './cadastro2/cadastro2.component';
+import { Cadastro4MusicoComponent } from './cadastro4-musico/cadastro4-musico.component';
+import { Cadastro4ContratanteComponent } from './cadastro4-contratante/cadastro4-contratante.component';
+import { Cadastro3MusicoComponent } from './cadastro3-musico/cadastro3-musico.component';
 
 const APP_ROUTES: Routes = [
 {path:"", redirectTo:"login", pathMatch:"full"},
 {path:"login", component:LoginComponent},
+{path:"cadastrar", component: CadastroComponent },
+{path:"cadastrar2", component: Cadastro2Component},
+{path:"cadastrar3contra", component: Cadastro3ContratanteComponent},
+{path:"cadastrar3musi", component: Cadastro3MusicoComponent},
+{path:"cadastrar4contra", component: Cadastro4ContratanteComponent},
+{path:"cadastrar4musi", component: Cadastro4MusicoComponent},
 {path:"menu", component:MenuComponent},
 {path:"perfil",component:PerfilMusicoComponent},
 {path:"shows", component:PerfilMusicoShowsComponent},
