795dc4a6e3b7b611e1882e56438639d8adf26841
Adonai Diógenes França Silva <adonaifranca@gmail.com>
2018-12-09 02:01:28 -0200

    Fiz o basico de validação do login e cadastro;

--- src/app/cadastro3-contratante/cadastro3-contratante.component.ts
+++ src/app/cadastro3-contratante/cadastro3-contratante.component.ts
@@ -9,15 +9,57 @@ export class Cadastro3ContratanteComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  //para validar o formulário
+  textError:boolean=false;
+  inputErrorEstab:boolean=false;
+  inputErrorPublico:boolean=false;
+
+  rota:String;
+
   estabelecimento: String;
   publico: number;
+
+
+  ngOnInit() {
+  }
+  
+  validacaoFormulario() {
+    //NOME ESTABELECIMENTO
+    if(this.estabelecimento == undefined){
+      this.inputErrorEstab=true;
+    }else{
+      this.inputErrorEstab=false;
+    }
+    //MÉDIA DE PÚBLICO
+    if(this.publico == undefined){
+      this.inputErrorPublico=true;
+    }else {
+      this.inputErrorPublico=false;
+    }
+    //campos vazios
+    if(this.estabelecimento==undefined || this.publico==undefined ){
+      this.textError=true;
+    }else{
+      this.textError=false;
+    }
+    if(this.inputErrorEstab || this.inputErrorPublico){
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+
   contraPart3(estabelecimento,publico){
-    var user = JSON.parse(localStorage.getItem("contratante"));
-    user.estabelecimento = estabelecimento;
-    user.publico = publico;
-    localStorage.setItem("contratante", JSON.stringify(user));
+    if(this.validacaoFormulario()){
+      console.log(this.rota);
+      this.rota="/cadastrar4contra";
+      console.log(this.rota);
+      var user = JSON.parse(localStorage.getItem("contratante"));
+      user.estabelecimento = this.estabelecimento;
+      user.publico = this.publico;
+      localStorage.setItem("contratante", JSON.stringify(user));
+    }
   }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { debug } from 'util';
+import { $ } from 'protractor';
 
 
 @Component({
@@ -11,45 +13,62 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-  email1: string
-  senha1: string
+  email1: string;
+  senha1: string;
   rota: string;
   act: boolean = false;
-  tipo1: boolean;
-  tipo2: boolean;
+  // tipo1: boolean = false;
+  // tipo2: boolean = false;
+  textError:boolean = false;
+  inputError:boolean = false;
 
+  
   ngOnInit() {
+    // localStorage.clear();
     var user = JSON.parse(localStorage.getItem("contratantes"));
-    var card = JSON.parse(localStorage.getItem("musicos"));
     var card = JSON.parse(localStorage.getItem("musicos"));
     console.log(user);
     console.log(card);
   }
 
-  onClickMusico(){
-    this.tipo1 = true;
-    this.tipo2 = false;
-    this.rota = "/perfilMusico";
+  // onClickMusico(){
+  //   this.tipo1 = true;
+  //   this.tipo2 = false;
+  // }
+  // onClickContratante(){
+  //   this.tipo1 = false;
+  //   this.tipo2 = true;
+  // }
+
+  loginError(){
+    this.textError = true;
+    this.inputError = true;
   }
-  onClickContratante(){
-    this.tipo1 = false;
-    this.tipo2 = true;
-    this.rota = "/perfilContratante";
-  }
+
   onSumit(email1,senha1){
     var contratantes = JSON.parse(localStorage.getItem("contratantes"));
     var musicos = JSON.parse(localStorage.getItem("musicos"));
     console.log(musicos);
-    for (let index = 0; index < musicos.length; index++) {
-      if(musicos[index].email==email1&&musicos[index].senha==senha1){
-        localStorage.setItem("atual", JSON.stringify(musicos[index]));
+
+    if(musicos != null){
+      for (let index = 0; index < musicos.length; index++) {
+        if(musicos[index].email==email1&&musicos[index].senha==senha1){
+          console.log(musicos[index]);
+          localStorage.setItem("atual", JSON.stringify(musicos[index]));
+          this.rota = "/perfilMusico";
+        }
       }
     }
-    for (let index = 0; index < contratantes.length; index++) {
-      if(contratantes[index].email==email1&&contratantes[index].senha==senha1){
-        console.log(contratantes[index]);
-        localStorage.setItem("atual", JSON.stringify(contratantes[index]));
+    if(contratantes != null){
+      for (let index = 0; index < contratantes.length; index++) {
+        if(contratantes[index].email==email1&&contratantes[index].senha==senha1){
+          console.log(contratantes[index]);
+          localStorage.setItem("atual", JSON.stringify(contratantes[index]));
+          this.rota = "/perfilContratante";
+        }
       }
     }
+    this.loginError();
+    console.log("Email ou senha está incorreto");
   }
 }
